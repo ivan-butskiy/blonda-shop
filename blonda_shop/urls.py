0659c958b5f0ff5835b03b98c30d75e03c7b9a4d
ivan_butskiy <butskiy1795@gmail.com>
2021-01-22 21:05:51 +0200

    seven

--- blonda_shop/urls.py
+++ blonda_shop/urls.py
@@ -11,10 +11,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('api/shop/', include('shop.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+#  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT,'show_indexes': False}),
-)

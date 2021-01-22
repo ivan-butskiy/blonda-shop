1e2d2816c5a2686cee26e5437a8a17f167873033
ivan_butskiy <butskiy1795@gmail.com>
2021-01-22 17:27:39 +0200

    please deploy it

--- blonda_shop/settings.py
+++ blonda_shop/settings.py
@@ -109,7 +109,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
